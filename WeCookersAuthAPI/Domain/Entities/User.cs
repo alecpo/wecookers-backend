da6bdd9b366aede8f98ac544bf52dbbc5968f6f8
alecpo <alessandro_vzp@hotmail.com>
2020-05-27 21:36:14 -0300

    initial commit

---
+++ WeCookersAuthAPI/Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace WeCookersAuthAPI.Domain.Entities
+{
+    [DataContract]
+    public class User
+    {
+        [Key]
+        [Column]
+        public Guid Id { get; set; }
+        
+        [Column]
+        public string Name { get; set; }
+
+        [Column]
+        public string Email { get; set; }
+
+        [Column]
+        public string Password { get; set; }
+
+        [Column]
+        public bool AllowedFlag { get; set; }
+
+    }
+}
---
+++ WeCookersAuthAPI/Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeCookersAuthAPI.Domain.Entities;
+
+namespace WeCookersAuthAPI.Infrastructure.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().HasKey(user => user.Id);
+        }
+    }
+}
